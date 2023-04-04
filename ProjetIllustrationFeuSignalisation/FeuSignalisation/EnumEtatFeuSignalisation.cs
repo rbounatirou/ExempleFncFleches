98ccf6b9fed94d1dabc8453a0b5b8fd3edb1eeaa
rbounatirou <92740209+rbounatirou@users.noreply.github.com>
2023-04-04 18:52:34 +0200

    First commit

---
+++ ProjetIllustrationFeuSignalisation/FeuSignalisation/EnumEtatFeuSignalisation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeuxSignalisations
+{
+    public enum EnumEtatFeuSignalisation
+    {
+        Rouge,
+        Orange,
+        Vert
+    }
+}
---
+++ ProjetIllustrationFeuSignalisation/FeuSignalisation/FeuSignalisation.cs
@@ -0,0 +1,67 @@
+﻿namespace FeuxSignalisations
+{
+    public class FeuSignalisation
+    {
+
+        private int tempsRougeMilliseconds;
+        private int tempsOrangeMilliseconds;
+        private int tempsVertMilliseconds;
+        private bool estAllimente;
+        private Thread sonThread;
+        private EnumEtatFeuSignalisation sonEtat;        
+
+        public delegate void OnStateChanged(FeuSignalisation sender, EnumEtatFeuSignalisation nouvelEtat);
+        public event OnStateChanged Event_OnStateChanged;
+        private EnumEtatFeuSignalisation SonEtat
+        {
+            set => changerEtatFeuSignalisation(value);
+        }
+
+
+        public FeuSignalisation(int tempsRouge, int tempsOrange, int tempsVert, EnumEtatFeuSignalisation etatInitial)
+        {
+            tempsRougeMilliseconds = tempsRouge;
+            tempsOrangeMilliseconds = tempsOrange;
+            tempsVertMilliseconds = tempsVert;
+            sonEtat = etatInitial;
+            estAllimente = true;
+            sonThread = new Thread(new ThreadStart(AlternerSignalisation));
+            sonThread.Start();
+        }
+
+        public FeuSignalisation(): this(20000, 5000, 20000, EnumEtatFeuSignalisation.Rouge){ } 
+        
+        private void changerEtatFeuSignalisation(EnumEtatFeuSignalisation nouvelEtat)
+        {
+            if (nouvelEtat == sonEtat)
+                return;
+            sonEtat = nouvelEtat;
+            if (Event_OnStateChanged != null)
+                Event_OnStateChanged(this, nouvelEtat);
+        }
+
+        public void AlternerSignalisation()
+        {
+            
+            while (estAllimente)
+            {
+                if (sonEtat == EnumEtatFeuSignalisation.Rouge)
+                {
+                    Thread.Sleep(tempsRougeMilliseconds);
+                    SonEtat = EnumEtatFeuSignalisation.Orange;
+                }
+                else if (sonEtat == EnumEtatFeuSignalisation.Orange)
+                {
+                    Thread.Sleep(tempsOrangeMilliseconds);
+                    SonEtat = EnumEtatFeuSignalisation.Vert;
+                } else
+                {
+                    Thread.Sleep(tempsVertMilliseconds);
+                    SonEtat = EnumEtatFeuSignalisation.Rouge;
+                }
+
+            }
+        }
+
+    }
+}
