6df1a654e6b3991888e7006e6b6a641b240a8ea1
rbounatirou <92740209+rbounatirou@users.noreply.github.com>
2023-04-10 22:18:53 +0200

    Ajout exercices fonction flechees

---
+++ Exercices/Exercices/Personne.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercices
+{
+    internal class Personne
+    {
+        public string nom { get; private set; }
+        public string prenom { get; private set; }
+        public int age { get; private set; }
+
+        public Personne(string nom, string prenom, int age)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.age = age;
+        }
+
+        public void ChangerNom(string nom)
+        {
+            this.nom = nom;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Nom: {0}, Prenom: {1}, Age: {2}", nom, prenom, age);
+        }
+    }
+}
---
+++ Exercices/Exercices/Program.cs
@@ -0,0 +1,206 @@
+﻿namespace Exercices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // NE PAS TOUCHER AU CONTENU DE LA METHODE MAIN
+            List<Personne> list = new List<Personne>();
+            list.Add(new Personne("Ananas", "Louis", 18));
+            list.Add(new Personne("Cerise", "Bertrand", 15));
+            list.Add(new Personne("Banane", "Gilles", 23));
+            list.Add(new Personne("Poire", "Marie-Helene", 16));
+         
+            
+            RQ1(list);
+            RQ2(list);
+            RQ3(list);
+            list.Add(new Personne("Poire", "Aurelie",16));
+            RQ4(list);
+            RQ5(list);
+            RQ6(list);
+
+            List<int> nbrs = new List<int> { 1, 5, 2, 3, 4, 8, 9 };
+            RQ7(nbrs);
+     
+        }
+
+        /*
+        Trie la liste en paramètre par ordre d'age.
+        Résultat attendu:
+
+        Louis>Bertrand>Gilles>Aurelie
+        */
+        static void RQ1(List<Personne> p)
+        {
+            Console.WriteLine("--Requete 1-- Age par ordre croissant");
+            // -- REQUETE ICI (1 ligne)
+
+
+            // ---- FIN REQUETE
+            AfficherListe(p);
+        }
+
+        
+        /*
+         Trie la liste par Nom par ordre alphabétique:
+        Résultat attendu:
+        Louis>Gilles>Bertrand>Marie-Helene
+        */
+        static void RQ2(List<Personne> p)
+        {
+            Console.WriteLine("--Requete 2-- Nom par ordre alphabetique");
+            // -- REQUETE ICI (1 ligne)
+
+
+            // ---- FIN REQUETE
+            AfficherListe(p);
+        }
+
+        /* Selectionner uniquement les personnes majeures
+         * Résultat:
+         * Louis > Gilles
+         */
+        static void RQ3(List<Personne> p)
+        {
+            Console.WriteLine("--Requete 3-- Personnes majeures uniquement");
+            List<Personne> personnesMajeures = new List<Personne>();
+            // -- REQUETE ICI (1 ligne)
+            
+
+            // ---- FIN REQUETE
+            AfficherListe(personnesMajeures);            
+        }
+        /* Trier les personnes par nom et en cas de nom identique par prenom
+          Résultat :
+            Louis>Gilles>Bertrand>Aurelie>Marie-Helene
+        */
+        static void RQ4(List<Personne> p)
+        {
+            Console.WriteLine("--Requete 4-- NOM et Prenom par ordre alphabetique ");
+            // -- REQUETE ICI
+
+
+            // ---- FIN REQUETE
+            AfficherListe(p);
+        }
+
+        /* Passer le nom de gens tout en majuscule (UpperCase) 
+         1 ligne de code seulement */
+        static void RQ5(List<Personne> p)
+        {
+            Console.WriteLine("--Requete 5-- NOM EN MAJUSCULE ");
+            // -- REQUETE ICI (1 ligne)
+           
+
+            // ---- FIN REQUETE
+            AfficherListe(p);
+        }
+
+        /* Somme des ages, sans utiliser IEnumerable
+         * Résultat attendu: 88
+         */
+        static void RQ6(List<Personne> p)
+        {
+            Console.WriteLine("--Requete 6-- Somme des ages ");
+            long age = 0;
+            // -- REQUETE ICI (1 ligne)
+
+
+            // ---- FIN REQUETE
+            Console.WriteLine(age);
+        }
+
+
+        /* Diviser par 2 les nombres fourni
+         * la partie après la virgule doit être présente */
+        static void RQ7(List<int> nb)
+        {
+            Console.WriteLine("-Requete 7-- Division par deux des nombres");
+            
+            // REQUETE ICI (2 lignes)
+            
+
+            // FIN REQUETE
+            
+        }
+
+
+        static void AfficherListe(List<Personne> p) => AfficherListe<Personne>(p);
+
+        static void AfficherListe<T>(List<T> p)
+        {
+            p.ForEach(x => Console.WriteLine(x));
+        }
+
+        #region solutions
+        static void Solution_RQ1(List<Personne> p)
+        {
+            Console.WriteLine("--Requete 1-- Age par ordre croissant");
+            // -- REQUETE ICI
+            p.Sort((a, b) => a.age - b.age);
+
+            // ---- FIN REQUETE
+            AfficherListe(p);
+        }
+
+        static void Solution_RQ2(List<Personne> p)
+        {
+            Console.WriteLine("--Requete 2--");
+            // -- REQUETE ICI
+            p.Sort((a, b) => a.nom.CompareTo(b.nom));
+            // ---- FIN REQUETE
+            AfficherListe(p);
+        }
+
+        static void Solution_RQ3(List<Personne> p)
+        {
+            Console.WriteLine("--Requete 3--");
+            List<Personne> personnesMajeures = new List<Personne>();
+            // -- REQUETE ICI
+            personnesMajeures = p.FindAll(x => x.age >= 18);
+            // ---- FIN REQUETE
+            AfficherListe(personnesMajeures);
+        }
+
+        static void Solution_RQ4(List<Personne> p)
+        {
+            Console.WriteLine("--Requete 4-- NOM et Prenom par ordre alphabetique ");
+            // -- REQUETE ICI (1 ligne)
+            p.Sort((a, b) => { int orderNom= a.nom.CompareTo(b.nom);
+                return (orderNom != 0 ? orderNom : a.prenom.CompareTo(b.prenom));
+            });
+            // ---- FIN REQUETE
+            AfficherListe(p);
+        }
+        static void Solution_RQ5(List<Personne> p)
+        {
+            Console.WriteLine("--Requete 5-- NOM EN MAJUSCULE ");
+            // -- REQUETE ICI
+            p.ForEach(x => x.ChangerNom(x.nom.ToUpper()));
+            // ---- FIN REQUETE
+            AfficherListe(p);
+        }
+
+        static void Solution_RQ6(List<Personne> p)
+        {
+            Console.WriteLine("--Requete 6-- Age total ");
+            long age = 0;
+            // -- REQUETE ICI
+            p.ForEach(x => age += x.age);
+            // ---- FIN REQUETE
+            Console.WriteLine(age);
+        }
+
+        static void Solution_RQ7(List<int> nb)
+        {
+            Console.WriteLine("-Requete 7-- Division par deux des nombres");
+
+            List<double> rt = new();
+            nb.ForEach(i=>rt.Add(i/2.0D));
+
+            AfficherListe<double>(rt);
+        }
+        #endregion
+    }
+}
